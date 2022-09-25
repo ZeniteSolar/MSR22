3ae24a518af66cf8c05dc5019f8998b299fc1f7d
ayresgit <ayres808@gmail.com>
2022-09-25 15:36:59 -0300

    Added gps files

---
+++ firmware/src/gps.h
@@ -0,0 +1,16 @@
+/**
+ * @file gps.h
+ *
+ * @defgroup U-Blox M8N Module gps control
+ *
+ * @brief Serial interface with gps in M8N module
+ *
+ */
+
+#ifndef GPS_H
+#define GPS_H
+
+
+
+#endif /* ifndef GPS_H */
+
