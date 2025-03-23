c1da7cb22df0ccf3beb010ae7ea46b1d1297c8e7
ayresgit <ayres808@gmail.com>
2025-03-23 16:57:18 -0300

    thorough adc init, corrected channels switch case, improved macros

--- firmware/src/adc.c
+++ firmware/src/adc.c
@@ -1,4 +1,29 @@
+
 #include "adc.h"
+
+volatile uint8_t adc_data_ready;
+#ifdef ADC_8BITS
+volatile uint8_t avg_adc0;
+volatile uint8_t avg_adc1;
+#else
+volatile uint16_t avg_adc0;
+volatile uint16_t avg_adc1;
+#endif
+
+volatile adc_cbuf_adc0_t cbuf_adc0;
+volatile adc_cbuf_adc1_t cbuf_adc1;
+
+// Coefficients for linearization, example coefficients
+static const int16_t adc0_a = 1;   
+static const int16_t adc0_b = 2;
+static const int16_t adc0_c = 3;
+static const int16_t adc1_a = 1;   
+static const int16_t adc1_b = 2;
+static const int16_t adc1_c = 3;
+
+// Define the linearization polynomial as a macro or function
+#define LINEARIZE_ADC0(x) ((adc0_a * (x) * (x)) + (adc0_b * (x)) + adc0_c)
+#define LINEARIZE_ADC1(x) ((adc1_a * (x) * (x)) + (adc1_b * (x)) + adc1_c)
 
 /**
  * @brief initializes all adc circular buffers.
@@ -7,7 +32,6 @@ void init_buffers(void)
 {
     CBUF_Init(cbuf_adc0);
     CBUF_Init(cbuf_adc1);
-    CBUF_Init(cbuf_adc2);
 }
 
 /**
@@ -22,12 +46,14 @@ uint8_t ma_adc0(void)
 #else
 uint16_t ma_adc0(void)
 #endif
-{   
+{
     uint16_t sum = 0;
-    for(uint8_t i = cbuf_adc0_SIZE; i; i--){
-        sum += CBUF_Get(cbuf_adc0, i);
+    for (uint8_t i = cbuf_adc0_SIZE; i; i--) {
+        uint16_t raw_value = CBUF_Get(cbuf_adc0, i);
+        uint16_t linearized_value = LINEARIZE_ADC0(raw_value);  // Apply polynomial
+        sum += linearized_value;
     }
-    avg_adc0 = sum >> cbuf_adc0_SIZE_2;
+    avg_adc0 = sum >> cbuf_adc0_SIZE_LOG2;
     return avg_adc0;
 }
 
@@ -43,34 +69,15 @@ uint8_t ma_adc1(void)
 #else
 uint16_t ma_adc1(void)
 #endif
-{   
+{
     uint16_t sum = 0;
-    for(uint8_t i = cbuf_adc1_SIZE; i; i--){
-        sum += CBUF_Get(cbuf_adc1, i);
+    for (uint8_t i = cbuf_adc1_SIZE; i; i--) {
+        uint16_t raw_value = CBUF_Get(cbuf_adc1, i);
+        uint16_t linearized_value = LINEARIZE_ADC1(raw_value);  // Apply polynomial
+        sum += linearized_value;
     }
-    avg_adc1 = sum >> cbuf_adc1_SIZE_2;
+    avg_adc1 = sum >> cbuf_adc1_SIZE_LOG2;
     return avg_adc1;
-}
-
-/**
-* @brief computes the average of a given adc channel 
-*
-* Ma = (1/N)*Summation of x[i] from i=0 to N, 
-* if N = 2^k, then Ma = (Summation of x[i] from i=0 to N) >> k
-*
-*/
-#ifdef ADC_8BITS
-uint8_t ma_adc2(void)
-#else
-uint16_t ma_adc2(void)
-#endif 
-{   
-    uint16_t sum = 0;
-    for(uint8_t i = cbuf_adc2_SIZE; i; i--){
-        sum += CBUF_Get(cbuf_adc2, i);
-    }
-    avg_adc2 = sum >> cbuf_adc2_SIZE_2;
-    return avg_adc2;
 }
 
 /**
@@ -92,7 +99,7 @@ uint8_t adc_select_channel(adc_channels_t __ch)
 void adc_init(void)
 {
     adc_data_ready = 0;
-    clr_bit(PRR0, PRADC);                           // Activates clock to adc
+    clr_bit(PRR, PRADC);                           // Activates clock to adc
 
     // configuracao do ADC
     PORTC   =   0b00000000;                         // disables pull-up for adcs pins
@@ -112,18 +119,32 @@ void adc_init(void)
             | (1 << ADTS0);
 
     adc_select_channel(ADC1);                       // Choose admux
-    ADCSRA  =   (1 << ADATE)                        // ADC Auto Trigger Enable
-            | (1 << ADIE)                           // ADC Interrupt Enable
-            | (1 << ADEN)                           // ADC Enable
-            | (1 << ADSC)                           // Do the first Start of Conversion
-            | (1 << ADPS2)                          // ADC Prescaller = 128;
-            | (1 << ADPS1)
-            | (1 << ADPS0);
-
+    ADCSRA  =   (1 << ADATE)    // ADC Auto Trigger Enable
+          | (1 << ADIE)     // ADC Interrupt Enable
+          | (1 << ADEN)     // ADC Enable
+          | (1 << ADSC)     // Do the first Start of Conversion
+#if ADC_TIMER_PRESCALER == 2
+          | (0 << ADPS2) | (0 << ADPS1) | (1 << ADPS0)  // Prescaler N=2
+#elif ADC_TIMER_PRESCALER == 4
+          | (0 << ADPS2) | (1 << ADPS1) | (0 << ADPS0)  // Prescaler N=4
+#elif ADC_TIMER_PRESCALER == 8
+          | (0 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)  // Prescaler N=8
+#elif ADC_TIMER_PRESCALER == 16
+          | (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0)  // Prescaler N=16
+#elif ADC_TIMER_PRESCALER == 32
+          | (1 << ADPS2) | (0 << ADPS1) | (1 << ADPS0)  // Prescaler N=32
+#elif ADC_TIMER_PRESCALER == 64
+          | (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0)  // Prescaler N=64
+#elif ADC_TIMER_PRESCALER == 128
+          | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)  // Prescaler N=128
+#else
+          | (1 << ADPS1) | (1 << ADPS0)  // Default to N=8 if no valid prescaler is set
+#endif
+;
 
 
     // configuracao do Timer TC0 --> TIMER DO ADC
-    clr_bit(PRR0, PRTIM0);                          // Activates clock to timer0
+    clr_bit(PRR, PRTIM0);                          // Activates clock to timer0
     //set_bit(DDRD, PD5);
     //set_bit(DDRD, PD6);
     TCCR0A  =   (1 << WGM01) | (0 << WGM00)         // Timer 0 in Mode 2 = CTC (clear on compare)
@@ -133,7 +154,7 @@ void adc_init(void)
             | (0 << FOC0A) | (0 << FOC0B)           // dont force outputs
             | (0 << CS02) | (1 << CS01) | (0 << CS00); // clock enabled, prescaller = 8
 
-	OCR0A  =    199;                                 // Valor para igualdade de comparacao A para frequencia de 5kHz
+	OCR0A  =    ADC_TOP_CTC;
     TIMSK0 |=   (1 << OCIE0A);                      // Ativa a interrupcao na igualdade de comparação do TC0 com OCR0A
 
     init_buffers();
@@ -146,7 +167,8 @@ void adc_init(void)
 ISR(ADC_vect){
     switch(ADC_CHANNEL){
         case ADC0:
-            VERBOSE_MSG_ADC(usart_send_string(" \tadc0: "));
+            // VERBOSE_MSG_ADC(usart_send_string(" \tadc0: "));
+            // VERBOSE_MSG_ADC(usart_send_uint16(ADC));
 #ifdef ADC_8BITS
             CBUF_Push(cbuf_adc0, ADCH); 
 #else
@@ -155,33 +177,30 @@ ISR(ADC_vect){
             ADC_CHANNEL++;
             break;
         case ADC1:
-            VERBOSE_MSG_ADC(usart_send_string(" \tadc1: "));
+            // VERBOSE_MSG_ADC(usart_send_string(" \tadc1: "));
 #ifdef ADC_8BITS
             CBUF_Push(cbuf_adc1, ADCH); 
 #else
             CBUF_Push(cbuf_adc1, ADC); 
 #endif
-            ADC_CHANNEL++;
+            // ADC_CHANNEL++;
+            
+            // Explicitly calling shared code instead of falling through
+            // __attribute__((fallthrough));
+            // Last channel logic 
+            ADC_CHANNEL = ADC0; // reset to first channel
+            adc_data_ready = 1; // Moving this into default might cause a false positive flag
+            // VERBOSE_MSG_ADC(usart_send_string("\n"));
             break;
-        case ADC2:
-            VERBOSE_MSG_ADC(usart_send_string(" \tadc2: "));
-#ifdef ADC_8BITS
-            CBUF_Push(cbuf_adc2, ADCH);
-#else
-            CBUF_Push(cbuf_adc2, ADC);
-#endif
-			ADC_CHANNEL++;
-            //break;
         default:
-            adc_data_ready = 1;
-            ADC_CHANNEL = ADC0;             // recycles
-            VERBOSE_MSG_ADC(usart_send_string("\n"));
+            ADC_CHANNEL = ADC0;
             break;
     }        
 #ifdef ADC_8BITS
     VERBOSE_MSG_ADC(usart_send_uint8(ADCH));
 #else
     VERBOSE_MSG_ADC(usart_send_uint16(ADC));
+    VERBOSE_MSG_ADC(usart_send_char('\n'));
 #endif
     adc_select_channel(ADC_CHANNEL);
 }
--- firmware/src/adc.h
+++ firmware/src/adc.h
@@ -4,95 +4,95 @@
  * @defgroup ADC    ADC Module
  *
  * @brief This module implements a simple ADC using a state machine to mux
- * between the adc channels.
+ * between the ADC channels.
  *
  */
 
 #ifndef _ADC_H_
 #define _ADC_H_
 
-#include "avr/io.h"
-#include "avr/interrupt.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 #include "conf.h"
 #include "dbg_vrb.h"
 #include "usart.h"
+
 #include "../lib/bit_utils.h"
 #include "../lib/cbuf.h"
 #include "../lib/log2.h"
 
-#define ADC_LAST_CHANNEL 3                  //*< quantity of channels used
-//#define ADC_8BITS
-//
-uint8_t adc_data_ready;
 
-typedef volatile enum adc_channels{ 
-    ADC0, ADC1 ,ADC2, ADC3, ADC4, ADC5  
-} adc_channels_t;                           //*< the adc_channel type 
-static adc_channels_t ADC_CHANNEL = ADC0;   //*< current chosen adc channel
+/** @brief Number of ADC channels used */
+#define ADC_LAST_CHANNEL 2
 
+/** @brief Flag indicating new ADC data is available */
+extern volatile uint8_t adc_data_ready;
+
+/** 
+ * @brief ADC channel selection enum
+ */
+typedef enum {
+    ADC0, ADC1, ADC2, ADC3, ADC4, ADC5
+} adc_channels_t;
+
+/** @brief Currently selected ADC channel */
+static volatile adc_channels_t ADC_CHANNEL = ADC0;
+
+/** 
+ * @brief Select an ADC channel
+ * @param __ch The ADC channel to select
+ * @return uint8_t Status of selection
+ */
 uint8_t adc_select_channel(adc_channels_t __ch);
+
+/** 
+ * @brief Initialize the ADC module 
+ */
 void adc_init(void);
 
-// MOVING AVERAGE BELOW //
-//
+/** @brief Initialize ADC moving average buffers */
 void init_buffers(void);
 
-#define cbuf_adc0_SIZE          64      // size of buffers
-#define cbuf_adc0_SIZE_2        6       // size of buffers in 2^n
-volatile struct cbuf_adc0{
-    uint8_t     m_getIdx;
-    uint8_t     m_putIdx;
+/** 
+ * @brief Circular buffer structure for ADC values 
+ */
+typedef struct {
+    uint8_t  m_getIdx;   /**< Read index */
+    uint8_t  m_putIdx;   /**< Write index */
 #ifdef ADC_8BITS
-    uint8_t     m_entry[cbuf_adc0_SIZE];
+    uint8_t  m_entry[cbuf_adc0_SIZE];  /**< Buffer entries (8-bit mode) */
 #else
-    uint16_t    m_entry[cbuf_adc0_SIZE];
+    uint16_t m_entry[cbuf_adc0_SIZE];  /**< Buffer entries (16-bit mode) */
+#endif
+} adc_cbuf_adc0_t;
+
+typedef struct {
+    uint8_t  m_getIdx;   /**< Read index */
+    uint8_t  m_putIdx;   /**< Write index */
+#ifdef ADC_8BITS
+    uint8_t  m_entry[cbuf_adc1_SIZE];  /**< Buffer entries (8-bit mode) */
+#else
+    uint16_t m_entry[cbuf_adc1_SIZE];  /**< Buffer entries (16-bit mode) */
+#endif
+} adc_cbuf_adc1_t;
+
+/** @brief Circular buffer instances for ADC0 and ADC1 */
+extern volatile adc_cbuf_adc0_t cbuf_adc0;
+extern volatile adc_cbuf_adc1_t cbuf_adc1;
+
+/** @brief Moving average and average value declarations */
+#ifdef ADC_8BITS
+extern volatile uint8_t avg_adc0;
+extern uint8_t ma_adc0(void);
+extern volatile uint8_t avg_adc1;
+extern uint8_t ma_adc1(void);
+#else
+extern volatile uint16_t avg_adc0;
+extern uint16_t ma_adc0(void);
+extern volatile uint16_t avg_adc1;
+extern uint16_t ma_adc1(void);
 #endif
 
-} cbuf_adc0;
-#ifdef ADC_8BITS
-uint8_t ma_adc0(void);
-uint8_t avg_adc0;
-#else
-uint16_t ma_adc0(void);
-uint16_t avg_adc0;
-#endif
 
-#define cbuf_adc1_SIZE          64      // size of buffers
-#define cbuf_adc1_SIZE_2        6       // size of buffers in 2^n
-volatile struct cbuf_adc1{
-    uint8_t     m_getIdx;
-    uint8_t     m_putIdx;
-#ifdef ADC_8BITS
-    uint8_t     m_entry[cbuf_adc1_SIZE];
-#else
-    uint16_t    m_entry[cbuf_adc1_SIZE];
-#endif
-} cbuf_adc1; 
-#ifdef ADC_8BITS
-uint8_t ma_adc1(void);
-uint8_t avg_adc1;
-#else
-uint16_t ma_adc1(void);
-uint16_t avg_adc1;
-#endif
-
-#define cbuf_adc2_SIZE          64      // size of buffers
-#define cbuf_adc2_SIZE_2        6       // size of buffers in 2^n
-volatile struct cbuf_adc2{
-    uint8_t     m_getIdx;
-    uint8_t     m_putIdx;
-#ifdef ADC_8BITS
-    uint8_t     m_entry[cbuf_adc2_SIZE];
-#else
-    uint16_t    m_entry[cbuf_adc2_SIZE];
-#endif
-} cbuf_adc2;  
-#ifdef ADC_8BITS
-uint8_t ma_adc2(void);
-uint8_t avg_adc2;
-#else
-uint16_t ma_adc2(void);
-uint16_t avg_adc2;
-#endif
-
-#endif /* ifndef _ADC_H_ */
+#endif /* _ADC_H_ */
